71b1d7d3f5b96226b3e54d603159159fa9c6eb55
rockysingh728208 <sagarkashyap9155@example.com>
2025-07-07 00:35:05 +0530

    Cleaned cache and added .gitignore

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connectDb=async()=>{
     try {
-        await mongoose.connect('mongodb+srv://chandan7061:chandan70@cluster0.agewmvu.mongodb.net')
+        await mongoose.connect('mongodb+srv://anu7061:anu70@cluster3.op1xkrd.mongodb.net')
         console.log("MongoDb connected successfully")
     } catch (error) {
         console.log(error)
