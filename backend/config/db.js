1a59e5971d5a527db3461f4ad5b359081511feb2
rockysingh728208 <sagarkashyap9155@example.com>
2025-07-07 09:15:10 +0530

    Cleaned cache and added .gitignore

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-export const connectDb=async()=>{
-    try {
-        await mongoose.connect('mongodb+srv://ruhi7061:ruhi70@cluster0.ascakek.mongodb.net')
-        console.log("MongoDb connected successfully")
-    } catch (error) {
-        console.log(error)
-    }
-}
+import dotenv from "dotenv";
+dotenv.config();
 
-
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDb connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
